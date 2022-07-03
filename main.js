d3bdc2f56390eeed5e0205c617ddec17459dc8f3
Maritxx <maritdevries1@live.nl>
2022-07-03 19:23:14 +0200

    Added const to js.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function submitRating(rating) {
-	divVoting = document.getElementById('voting-component');
-	divSubmitted = document.getElementById('submitted-response');
-	output = document.getElementById('output');
+	const divVoting = document.getElementById('voting-component');
+	const divSubmitted = document.getElementById('submitted-response');
+	const output = document.getElementById('output');
 
 	if (!rating) {
 		alert('Submit a rating!');
