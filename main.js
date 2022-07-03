16830cd9013997b8f48bdda5fab18458dab488e0
Maritxx <maritdevries1@live.nl>
2022-07-03 18:45:16 +0200

    Fix JS pls?

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function submitRating(rating) {
-	divSelector = document.getElementByClassName('rating-component__form');
-	divResponse = document.getElementByClassName('rating-component__submitted');
-	ratingOutput = document.getElementById('ratingOutput');
+	divSelector = document.getElementsByClassName('rating-component__form');
+	divResponse = document.getElementsByClassName('rating-component__submitted');
+	output = document.getElementsById('output');
 
 	if (!rating) {
 		alert('Submit a rating!');
 	}	else {
 		divSelector.classList.add('hidden');
 		divResponse.classList.remove('hidden');
-		ratingOutput.value = rating;
+		output.value = rating;
 	}
 }
