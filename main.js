b46c9e1af39fc7b6c6fe368dd35f1eab49cd26db
Maritxx <maritdevries1@live.nl>
2022-07-03 18:16:24 +0200

    Changed JS.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function submitRating(rating) {
 	divSelector = document.getElementsByClassName('rating-component__form');
 	divResponse = document.getElementByClassName('rating-component__submitted');
-	output = document.getElementById('ratingOutput');
+	ratingOutput = document.getElementById('ratingOutput');
 
 	if (!rating) {
 		alert('Submit a rating!');
 	}	else {
 		divSelector.classList.add('hidden');
 		divResponse.classList.remove('hidden');
-		output.value = rating;
+		ratingOutput.value = rating;
 	}
 }
