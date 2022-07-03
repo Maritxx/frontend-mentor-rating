9480cde051d0213d3297799ad817c763dc569a3e
Maritxx <maritdevries1@live.nl>
2022-07-03 17:50:21 +0200

    Attempt at Javascript instead of php

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function submitRating(rating) {
+	divSelector = document.getElementsByClassName("rating-component__form");
+	divResponse = document.getElementByClassName("rating-component__submitted");
+	output = document.getElementById("output");
 
+	if (!rating) {
+		alert("Submit a rating!");
+	}	else {
+		divSelector.classList.add('hidden');
+		divResponse.classList.remove('hidden');
+		output.value = rating;
+	}
+}
